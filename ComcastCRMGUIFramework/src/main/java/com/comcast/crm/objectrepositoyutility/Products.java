6233657624e3a12b77170839eab8d323e8ed7fba
Lenovo <Lenovo@DESKTOP-QJ8EISD>
2024-05-28 10:18:01 +0530

    Product , Added new POM class

---
+++ ComcastCRMGUIFramework/src/main/java/com/comcast/crm/objectrepositoyutility/Products.java
@@ -0,0 +1,10 @@
+package com.comcast.crm.objectrepositoyutility;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class Products {
+
+	@FindBy(xpath="//input[@alt='Create Product...']")
+	private WebElement createProductImgBtn;
+}
